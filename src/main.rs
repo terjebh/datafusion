125a50739d94e4fad1532991307162500dcab360
Terje Berg-Hansen <terje@itfakultetet.no>
2025-01-03 16:03:38 +0100

    Eksempel på Datafusion

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+use std::env;
 use datafusion::prelude::*;
 
 #[tokio::main]
 async fn main() -> datafusion::error::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
   // register the table
   let ctx = SessionContext::new();
-  ctx.register_csv("dogs", "/home/terje/utvikling/rust/datafusion/dogs.csv", CsvReadOptions::new()).await?;
+  ctx.register_csv("tabell", &file_path, CsvReadOptions::new()).await?;
 
   // create a plan to run a SQL query
-  let df = ctx.sql("SELECT * from dogs").await?;
+  let df = ctx.sql("SELECT * from tabell").await?;
 
   // execute and print results
   df.show().await?;
